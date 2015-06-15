5dbf35161e16b66ba006d5261bde5328c7daa3bc
rycwilson <rycwilson@gmail.com>
2015-06-15 16:27:29 -0700

    user signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,29 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:email].present? && params[:password].present?
+      # email and password entered, ok
+      if User.find_by(email:params[:email])
+        # found a user, ok
+        @user = User.confirm({email:params[:email], password:params[:password]})
+        if @user
+          # user authorized, ok
+          login @user
+          redirect_to user_path(@user)
+        else
+          # bad password
+          # display the login page again
+          render :new
+        end
+      else
+        # user not found
+        # have them sign up
+        redirect_to signup_path
+      end
+    else
+      # incomplete login fields
+      render :new
+    end
   end
 
   def delete
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,45 @@
 class UsersController < ApplicationController
 
+  #
+  # GET /signup
+  #
   def new
     @user = User.new
+    render :new
   end
 
+  #
+  # GET /user/:id
+  #
   def show
+    @user = User.find params[:id]
+    render :show
+  end
+
+  #
+  # POST /users
+  #
+  def create
+    binding.pry
+    @user = User.new user_params
+    if @user.save
+      binding.pry
+      login(@user)
+      redirect_to user_path(@user)
+    else
+      binding.pry
+      @errors = @user.errors.messages
+      render :new
+    end
   end
 
   def edit
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :home_city, :email, :email_confirmation, :password, :password_confirmation)
+  end
+
 end
