31c3cdf969cb6245bd3a059af5b40bb5d06efaa0
nathanallen <thenathanator@gmail.com>
2015-06-15 22:14:37 -0700

    test user has_secure_password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # SETUP
+  before(:all) do
+    @params = {
+                  :first_name => "Sweeny",
+                  :last_name => "Todd",
+                  :password => "12345",
+                  :email => "st@barbershop.com",
+                  :home_city => "London"
+              }
+    # create a new user for use in all tests
+    @user = User.create(@params)
+  end
+
+  context "has_secure_password" do
+    it "does something fancy with the password" do
+      expect(@user.password_digest).not_to eq @params[:password]
+    end
+  end
+
+  # TEAR DOWN
+  after(:all) do
+    # delete the new user, so we don't clutter the test database
+    @user.destroy
+  end
+
 end
