331451ea70b7c062c645320b1b8fa738327cbab9
nathanallen <thenathanator@gmail.com>
2015-06-15 23:24:03 -0700

    test profile view shows correct user info

---
+++ spec/views/users/show.html.erb_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.describe "the user profile page" do
+
+  before(:all) do
+    @params = {
+                  :first_name => "Sweeny",
+                  :last_name => "Todd",
+                  :password => "12345",
+                  :email => "st@barbershop.com",
+                  :home_city => "London"
+              }
+    # create a new user for use in all tests
+    @user = User.create(@params)
+  end
+
+  it "displays basic profile information" do
+    assign(:user, @user)
+
+    render :template => "users/show.html.erb"
+
+    expect(rendered).to match /Sweeny/
+    expect(rendered).to match /Todd/
+    expect(rendered).to match /London/
+    expect(rendered).to match Date.today.to_s
+  end
+
+  # TEAR DOWN
+  after(:all) do
+    # delete the new user, so we don't clutter the test database
+    @user.destroy
+  end
+end
