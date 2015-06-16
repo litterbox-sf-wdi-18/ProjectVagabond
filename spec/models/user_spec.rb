d7ffff259b44aac33a182a60543cada10f98a64f
nathanallen <thenathanator@gmail.com>
2015-06-15 22:16:48 -0700

    test User#confirm passwords

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,17 @@ RSpec.describe User, :type => :model do
     end
   end
 
+  context "User#confirm" do
+    it "confirms correct password" do
+      expect( User.confirm(@params) ).to eq @user
+    end
+
+    it "rejects incorrect password"  do
+      expect( User.confirm({:email => @params[:email], :password => "BAD"}) ).to be false
+    end
+  end
+
+
   # TEAR DOWN
   after(:all) do
     # delete the new user, so we don't clutter the test database
