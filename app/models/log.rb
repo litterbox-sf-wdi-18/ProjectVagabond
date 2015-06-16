53c44b6f70b575344242ec6012d8bed89d0b63ca
vikashiscool <vikash_parekh@yahoo.com>
2015-06-16 16:29:51 -0700

    Add image to logs via paperclip

--- app/models/log.rb
+++ app/models/log.rb
@@ -9,5 +9,6 @@ class Log < ActiveRecord::Base
   #Paperclip
   has_attached_file :image, :styles => { :medium => "300x300>" }, :default_url => "/images/:style/missing.png" 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
 
 end
